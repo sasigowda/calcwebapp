7ccb4f98cc79b931ce4a56f85f34ec91410c2c69
Husain Dehnuwala <dehnuwala.husain@gmail.com>
2020-02-26 21:52:07 +0530

    changed to multiply

--- src/main/java/com/cruds/service/CalculatorService.java
+++ src/main/java/com/cruds/service/CalculatorService.java
@@ -2,9 +2,9 @@ package com.cruds.service;
 
 public class CalculatorService {
 	
-	public static int add(int x, int y)
+	public static int mul(int x, int y)
 	{
-		return x + y;
+		return x * y;
 	}
 
 }
